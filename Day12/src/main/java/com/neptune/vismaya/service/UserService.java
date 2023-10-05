54cbef47ff98c958989b89956f90b06a42fa5019
Vismaya S <104690099+Vismaya24@users.noreply.github.com>
2023-10-05 10:40:09 +0530

    Add files via upload

---
+++ Day12/src/main/java/com/neptune/vismaya/service/UserService.java
@@ -0,0 +1,18 @@
+package com.neptune.vismaya.service;
+
+import java.util.List;
+
+import com.neptune.vismaya.dto.request.UserRequestDto;
+import com.neptune.vismaya.dto.response.UserResponseDto;
+
+public interface UserService {
+
+	boolean saveUser(UserRequestDto userRequest);
+
+	List<UserResponseDto> getAllUser();
+
+	UserResponseDto updateUser(UserRequestDto userRequest, int id);
+
+	boolean deleteUser(int id);
+	
+}
